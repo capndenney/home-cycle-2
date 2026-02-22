6f17e6cec8cbad007a22b1768b0a2c3ad7c197e9
Mike Denney <mike.a.denney@gmail.com>
2026-02-21 20:05:50 -0600

    added Household model with getters and setters

--- data/src/main/java/com/home_cycle/data/models/Household.java
+++ data/src/main/java/com/home_cycle/data/models/Household.java
@@ -1,4 +1,50 @@
 package com.home_cycle.data.models;
 
 public class Household {
+    private int id;
+    private int createdBy;
+    private long createdAt; // TODO: Confirm correct data type for date
+    private String notes;
+
+    public Household() {
+    }
+
+    public Household(int id, int createdBy, long createdAt, String notes) {
+        this.id = id;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
