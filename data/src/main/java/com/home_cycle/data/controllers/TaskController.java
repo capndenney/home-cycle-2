f56aa56539aa9e62b139048b5f4196c49cf36b20
Mike Denney <mike.a.denney@gmail.com>
2026-02-21 22:21:03 -0600

    added get mappings for TaskController, added maven dependency

--- data/src/main/java/com/home_cycle/data/controllers/TaskController.java
+++ data/src/main/java/com/home_cycle/data/controllers/TaskController.java
@@ -1,24 +1,54 @@
 package com.home_cycle.data.controllers;
 
+import com.home_cycle.data.dto.request.TaskDTO;
+import com.home_cycle.data.models.Task;
+import com.home_cycle.data.models.User;
 import com.home_cycle.data.repositories.TaskRepository;
+import com.home_cycle.data.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
-    TaskRepository taskRepository;
+    private TaskRepository taskRepository;
 
     // Get all tasks
+    @GetMapping("")
+    public ResponseEntity<?> getAllTasks() {
+        List<Task> allTasks = taskRepository.findAll();
+        return ResponseEntity.ok(allTasks);
+    }
 
     // Get specific task by id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTaskById(@PathVariable int id) {
+        return taskRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
     // Add new task
+    // TODO: Determine connection point to front end and link found user ID to task creation
+//    @PostMapping(value = "/newtask")
+//    public ResponseEntity<?> createTask(@Validated @RequestBody TaskDTO taskDTO) {
+//        User user = UserRepository.findById(taskDTO.getCreatedBy()).orElse(null);
+//        Task task = new Task()
+//    }
 
     // Update existing task
 
+
     // Delete task by id
+
+
 }
--- data/src/main/java/com/home_cycle/data/dto/request/TaskDTO.java
+++ data/src/main/java/com/home_cycle/data/dto/request/TaskDTO.java
@@ -1,4 +1,105 @@
 package com.home_cycle.data.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class TaskDTO {
+    @NotBlank(message = "Task Title is required")
+    @Size(max = 100, message = "Task Title must be less than 100 characters")
+    String title;
+
+    private String description;
+    private int householdId;
+    @NotNull(message = "Due Date is required")
+    private long dueDate; // TODO: Confirm correct data type for date
+    private boolean completed;
+    private long completedAt; // TODO: Confirm correct data type for date
+    private int recurrence; // TODO: Date type definition
+    private int completedBy;
+    private int createdBy;
+
+    public TaskDTO(String title, String description, int householdId, long dueDate, boolean completed, long completedAt, int recurrence, int completedBy, int createdBy) {
+        this.title = title;
+        this.description = description;
+        this.householdId = householdId;
+        this.dueDate = dueDate;
+        this.completed = completed;
+        this.completedAt = completedAt;
+        this.recurrence = recurrence;
+        this.completedBy = completedBy;
+        this.createdBy = createdBy;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getHouseholdId() {
+        return householdId;
+    }
+
+    public void setHouseholdId(int householdId) {
+        this.householdId = householdId;
+    }
+
+    public long getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(long dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public long getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(long completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public int getRecurrence() {
+        return recurrence;
+    }
+
+    public void setRecurrence(int recurrence) {
+        this.recurrence = recurrence;
+    }
+
+    public int getCompletedBy() {
+        return completedBy;
+    }
+
+    public void setCompletedBy(int completedBy) {
+        this.completedBy = completedBy;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
 }
