036699f71ffe05a21d7bedc75f050918e3ad706d
Mike Denney <mike.a.denney@gmail.com>
2026-02-21 21:16:25 -0600

    framed controllers

--- data/src/main/java/com/home_cycle/data/controllers/TaskController.java
+++ data/src/main/java/com/home_cycle/data/controllers/TaskController.java
@@ -1,4 +1,24 @@
 package com.home_cycle.data.controllers;
 
+import com.home_cycle.data.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    // Get all tasks
+
+    // Get specific task by id
+
+    // Add new task
+
+    // Update existing task
+
+    // Delete task by id
 }
--- data/src/main/java/com/home_cycle/data/controllers/UserController.java
+++ data/src/main/java/com/home_cycle/data/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.home_cycle.data.controllers;
 
+import com.home_cycle.data.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    // DO NOT add "get all users" for security
+
+    // Get specific user information by ID
+
+    // Add new user to database
+
+    // Update user details
+
+    // Delete User by ID
 }
