8eb3483d53b8dd0d1e6a78f8607f71b47b16dc72
Mike Denney <mike.a.denney@gmail.com>
2026-03-02 21:26:35 -0600

    added user services for encoding and auth

--- data/src/main/java/com/home_cycle/data/services/TokenBlacklistService.java
+++ data/src/main/java/com/home_cycle/data/services/TokenBlacklistService.java
@@ -1,4 +1,20 @@
 package com.home_cycle.data.services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
 public class TokenBlacklistService {
+
+    private Set<String> blacklistedTokens = new HashSet<>();
+
+    public void addTokenToBlacklist(String token) {
+        blacklistedTokens.add(token);
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return blacklistedTokens.contains(token);
+    }
 }
---
+++ data/src/main/java/com/home_cycle/data/services/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.home_cycle.data.services;
+
+import com.home_cycle.data.dto.request.UserDTO;
+import com.home_cycle.data.models.User;
+import com.home_cycle.data.repositories.UserRepository;
+import jakarta.persistence.EntityExistsException;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository, PasswordEncoder encoder) {
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public UserDTO createNewUser (UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
+            throw new EntityExistsException("User with email " + userDTO.getEmail() + " already exists");
+        }
+        userDTO.setPassword(encoder.encode(userDTO.getPassword()));
+        User user = mapToProfileEntity(userDTO);
+        User savedUser = userRepository.save(user);
+        return mapToProfileDTO(savedUser);
+    }
+
+    private UserDTO mapToProfileDTO(User profile) {
+        return modelMapper.map(profile, UserDTO.class);
+    }
+
+    private User mapToProfileEntity(UserDTO profileDTO) {
+        return modelMapper.map(profileDTO, User.class);
+    }
+}
